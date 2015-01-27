0b0f2592404963218c094f96688893cc7201d849
Tomas Majer <tomasmajer@gmail.com>
2015-01-27 10:57:03 +0100

    Added tests

---
+++ tests/MailCriteriaTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+
+use Tomaj\ImapMailDownloader\MailCriteria;
+
+class MailCriteriaTest extends PHPUnit_Framework_TestCase
+{
+	public function testDefault()
+	{
+		$criteria = new MailCriteria();
+		$this->assertEquals('ALL', $criteria->getSearchString());
+	}
+
+	public function testFrom()
+	{
+		$criteria = new MailCriteria();
+		$criteria->setFrom('a@a.sk');
+		$this->assertEquals("FROM 'a@a.sk'", $criteria->getSearchString());
+	}
+
+	public function testAllCriteria()
+	{
+		$criteria = new MailCriteria();
+		$criteria->setFrom('b@b.sk');
+		$criteria->setKeyword('testKeyword');
+		$criteria->setUnseen(true);
+		$criteria->setSince('2014-01-05 14:56:11');
+		$criteria->setSubject('my Subject');
+
+		$this->assertEquals("FROM 'b@b.sk' KEYWORD 'testKeyword' UNSEEN SUBJECT 'my Subject' SINCE '2014-01-05 14:56:11'", $criteria->getSearchString());
+	}
+}
