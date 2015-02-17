40ff4a5faa6921242cd50aef59146c5edb76f1e7
Tomas Majer <tomasmajer@gmail.com>
2015-02-17 09:46:11 +0100

    Updated condition if no emails found

--- src/Downloader.php
+++ src/Downloader.php
@@ -37,19 +37,21 @@ class Downloader
 
         $emails = $this->fetchEmails($mailbox, $criteria);
 
-        foreach ($emails as $emailIndex) {
-            $overview = imap_fetch_overview($mailbox, $emailIndex, 0);
-            $message = imap_body($mailbox, $emailIndex);
+        if ($emails) {
+            foreach ($emails as $emailIndex) {
+                $overview = imap_fetch_overview($mailbox, $emailIndex, 0);
+                $message = imap_body($mailbox, $emailIndex);
 
-            $email = new Email($overview, $message);
+                $email = new Email($overview, $message);
 
-            $processed = $callback($email);
+                $processed = $callback($email);
 
-            if ($processed) {
-                $res = imap_mail_move($mailbox, $emailIndex, $this->processedFolder);
-                if (!$res) {
-                    throw new \Exception("Unexpected error: Cannot move email to ");
-                    break;
+                if ($processed) {
+                    $res = imap_mail_move($mailbox, $emailIndex, $this->processedFolder);
+                    if (!$res) {
+                        throw new \Exception("Unexpected error: Cannot move email to ");
+                        break;
+                    }
                 }
             }
         }
