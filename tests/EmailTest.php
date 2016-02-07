093212a9e1df4d07d8f2cd0e8f21d28bc967723f
Tomas Majer <tomasmajer@gmail.com>
2016-02-07 21:21:23 +0100

    Fixed indent in tests

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -6,86 +6,86 @@ use Tomaj\ImapMailDownloader\Email;
 
 class EmailTest extends PHPUnit_Framework_TestCase
 {
-	public function testCreationWithouOptionalAttributes()
-	{
-		$data = new stdClass;
-		$data->from = 'from@asdsad.sk';
-		$data->to = 'asdsad@adsad.sk';
-		$data->date = '2014-01-02 14:34';
-		$data->message_id = 'sa09uywqet09u3t';
-		$data->size = 125;
-		$data->uid = '236-0982369034856';
-		$data->msgno = 4125;
-		$data->recent = 1;
-		$data->flagged = 0;
-		$data->answered = 1;
-		$data->deleted = 0;
-		$data->seen = 1;
-		$data->draft = 1;
+    public function testCreationWithouOptionalAttributes()
+    {
+        $data = new stdClass;
+        $data->from = 'from@asdsad.sk';
+        $data->to = 'asdsad@adsad.sk';
+        $data->date = '2014-01-02 14:34';
+        $data->message_id = 'sa09uywqet09u3t';
+        $data->size = 125;
+        $data->uid = '236-0982369034856';
+        $data->msgno = 4125;
+        $data->recent = 1;
+        $data->flagged = 0;
+        $data->answered = 1;
+        $data->deleted = 0;
+        $data->seen = 1;
+        $data->draft = 1;
         $data->text = "text";
 
-		$body = 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg';
+        $body = 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg';
 
-		$email = new Email(array($data), $body);
+        $email = new Email(array($data), $body);
 
-		$this->assertEquals($email->getFrom(), 'from@asdsad.sk');
-		$this->assertEquals($email->getTo(), 'asdsad@adsad.sk');
-		$this->assertEquals($email->getDate(), '2014-01-02 14:34');
-		$this->assertEquals($email->getMessageId(), 'sa09uywqet09u3t');
-		$this->assertEquals($email->getReferences(), null);
-		$this->assertEquals($email->getInReplyTo(), null);
-		$this->assertEquals($email->getSize(), 125);
-		$this->assertEquals($email->getUid(), '236-0982369034856');
-		$this->assertEquals($email->getMsgNo(), 4125);
-		$this->assertEquals($email->getRecent(), 1);
-		$this->assertEquals($email->getFlagged(), 0);
-		$this->assertEquals($email->getAnswered(), 1);
-		$this->assertEquals($email->getDeleted(), 0);
-		$this->assertEquals($email->getSeen(), 1);
-		$this->assertEquals($email->getDraft(), 1);
+        $this->assertEquals($email->getFrom(), 'from@asdsad.sk');
+        $this->assertEquals($email->getTo(), 'asdsad@adsad.sk');
+        $this->assertEquals($email->getDate(), '2014-01-02 14:34');
+        $this->assertEquals($email->getMessageId(), 'sa09uywqet09u3t');
+        $this->assertEquals($email->getReferences(), null);
+        $this->assertEquals($email->getInReplyTo(), null);
+        $this->assertEquals($email->getSize(), 125);
+        $this->assertEquals($email->getUid(), '236-0982369034856');
+        $this->assertEquals($email->getMsgNo(), 4125);
+        $this->assertEquals($email->getRecent(), 1);
+        $this->assertEquals($email->getFlagged(), 0);
+        $this->assertEquals($email->getAnswered(), 1);
+        $this->assertEquals($email->getDeleted(), 0);
+        $this->assertEquals($email->getSeen(), 1);
+        $this->assertEquals($email->getDraft(), 1);
 
-		$this->assertEquals($email->getBody(), 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg');
-	}
+        $this->assertEquals($email->getBody(), 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg');
+    }
 
-	public function testCreationWithAllAttributes()
-	{
-		$data = new stdClass;
-		$data->from = 'from@asdsad.sk';
-		$data->to = 'asdsad@adsad.sk';
-		$data->date = '2014-01-02 14:34';
-		$data->message_id = 'sa09uywqet09u3t';
-		$data->references = 'asdas09uyfei9f';
-		$data->in_reply_to = '135325325325';
-		$data->size = 125;
-		$data->uid = '236-0982369034856';
-		$data->msgno = 4125;
-		$data->recent = 1;
-		$data->flagged = 0;
-		$data->answered = 1;
-		$data->deleted = 0;
-		$data->seen = 1;
-		$data->draft = 1;
+    public function testCreationWithAllAttributes()
+    {
+        $data = new stdClass;
+        $data->from = 'from@asdsad.sk';
+        $data->to = 'asdsad@adsad.sk';
+        $data->date = '2014-01-02 14:34';
+        $data->message_id = 'sa09uywqet09u3t';
+        $data->references = 'asdas09uyfei9f';
+        $data->in_reply_to = '135325325325';
+        $data->size = 125;
+        $data->uid = '236-0982369034856';
+        $data->msgno = 4125;
+        $data->recent = 1;
+        $data->flagged = 0;
+        $data->answered = 1;
+        $data->deleted = 0;
+        $data->seen = 1;
+        $data->draft = 1;
 
-		$body = 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg';
+        $body = 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg';
 
-		$email = new Email(array($data), $body);
+        $email = new Email(array($data), $body);
 
-		$this->assertEquals($email->getFrom(), 'from@asdsad.sk');
-		$this->assertEquals($email->getTo(), 'asdsad@adsad.sk');
-		$this->assertEquals($email->getDate(), '2014-01-02 14:34');
-		$this->assertEquals($email->getMessageId(), 'sa09uywqet09u3t');
-		$this->assertEquals($email->getReferences(), 'asdas09uyfei9f');
-		$this->assertEquals($email->getInReplyTo(), '135325325325');
-		$this->assertEquals($email->getSize(), 125);
-		$this->assertEquals($email->getUid(), '236-0982369034856');
-		$this->assertEquals($email->getMsgNo(), 4125);
-		$this->assertEquals($email->getRecent(), 1);
-		$this->assertEquals($email->getFlagged(), 0);
-		$this->assertEquals($email->getAnswered(), 1);
-		$this->assertEquals($email->getDeleted(), 0);
-		$this->assertEquals($email->getSeen(), 1);
-		$this->assertEquals($email->getDraft(), 1);
+        $this->assertEquals($email->getFrom(), 'from@asdsad.sk');
+        $this->assertEquals($email->getTo(), 'asdsad@adsad.sk');
+        $this->assertEquals($email->getDate(), '2014-01-02 14:34');
+        $this->assertEquals($email->getMessageId(), 'sa09uywqet09u3t');
+        $this->assertEquals($email->getReferences(), 'asdas09uyfei9f');
+        $this->assertEquals($email->getInReplyTo(), '135325325325');
+        $this->assertEquals($email->getSize(), 125);
+        $this->assertEquals($email->getUid(), '236-0982369034856');
+        $this->assertEquals($email->getMsgNo(), 4125);
+        $this->assertEquals($email->getRecent(), 1);
+        $this->assertEquals($email->getFlagged(), 0);
+        $this->assertEquals($email->getAnswered(), 1);
+        $this->assertEquals($email->getDeleted(), 0);
+        $this->assertEquals($email->getSeen(), 1);
+        $this->assertEquals($email->getDraft(), 1);
 
-		$this->assertEquals($email->getBody(), 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg');
-	}
+        $this->assertEquals($email->getBody(), 'asf098ywetoiuwhegt908weg ewfg dsyfg089dsyfg');
+    }
 }
--- tests/MailCriteriaTest.php
+++ tests/MailCriteriaTest.php
@@ -6,18 +6,18 @@ use Tomaj\ImapMailDownloader\MailCriteria;
 
 class MailCriteriaTest extends PHPUnit_Framework_TestCase
 {
-	public function testDefault()
-	{
-		$criteria = new MailCriteria();
-		$this->assertEquals('ALL', $criteria->getSearchString());
-	}
+    public function testDefault()
+    {
+        $criteria = new MailCriteria();
+        $this->assertEquals('ALL', $criteria->getSearchString());
+    }
 
-	public function testFrom()
-	{
-		$criteria = new MailCriteria();
-		$criteria->setFrom('a@a.sk');
-		$this->assertEquals('FROM "a@a.sk"', $criteria->getSearchString());
-	}
+    public function testFrom()
+    {
+        $criteria = new MailCriteria();
+        $criteria->setFrom('a@a.sk');
+        $this->assertEquals('FROM "a@a.sk"', $criteria->getSearchString());
+    }
 
     public function testText()
     {
@@ -26,15 +26,15 @@ class MailCriteriaTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('TEXT "test text"', $criteria->getSearchString());
     }
 
-	public function testAllCriteria()
-	{
-		$criteria = new MailCriteria();
-		$criteria->setFrom('b@b.sk');
-		$criteria->setKeyword('testKeyword');
-		$criteria->setUnseen(true);
-		$criteria->setSince('2014-01-05 14:56:11');
-		$criteria->setSubject('my Subject');
+    public function testAllCriteria()
+    {
+        $criteria = new MailCriteria();
+        $criteria->setFrom('b@b.sk');
+        $criteria->setKeyword('testKeyword');
+        $criteria->setUnseen(true);
+        $criteria->setSince('2014-01-05 14:56:11');
+        $criteria->setSubject('my Subject');
 
-		$this->assertEquals('FROM "b@b.sk" KEYWORD "testKeyword" UNSEEN SUBJECT "my Subject" SINCE "2014-01-05 14:56:11"', $criteria->getSearchString());
-	}
+        $this->assertEquals('FROM "b@b.sk" KEYWORD "testKeyword" UNSEEN SUBJECT "my Subject" SINCE "2014-01-05 14:56:11"', $criteria->getSearchString());
+    }
 }
